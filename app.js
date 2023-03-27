c97468c160319ed47f97b5ab135e7da66fa798c0
thomsa5566 <fain73@hotmail.com>
2023-03-27 17:12:51 +0800

    Add OpenAI

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const line = require("@line/bot-sdk");
+const { Configuration, OpenAIApi } = require("openai");
+
 const express = require("express");
 
 // create LINE SDK config from env variables
@@ -8,6 +10,11 @@ const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // create LINE SDK client
 const client = new line.Client(config);
@@ -18,7 +25,7 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post("/callback", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
@@ -28,21 +35,20 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text,
+  });
+
   // create a echoing text message
-  const echo = { type: "text", text: event.message.text };
+  const echo = { type: "text", text: completion.data.choices[0].text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
